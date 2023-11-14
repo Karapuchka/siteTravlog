ef33b895a702197ca67ded4bebe02b909dcd2adf
Karapuchka <karapu4ka2@gmail.com>
2023-11-14 23:01:04 +0300

    .

---
+++ public/js/writePost.js
@@ -0,0 +1,12 @@
+const inputs = document.getElementsByClassName('js-input');
+const btn = document.getElementById('btn-sub');
+
+btn.onclick = (e)=>{
+    for (let i = 0; i < inputs.length - 1; i++) {
+        if(inputs[i].value == ''){
+            e.preventDefault();
+            alert('Заполните все поля!');
+            break;
+        }        
+    }
+}
